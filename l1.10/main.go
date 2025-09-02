531da6d40e7fef978ba6d86143e1a6c69efca3fc
vladk39rus <vladk39rus@gmail.com>
2025-09-02 14:57:17 +0300

    ds

--- l1.10/main.go
+++ l1.10/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	Temp := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+
+	fmt.Println(group(Temp))
+}
+
+func group(temp []float64) map[int][]float64 {
+	result := make(map[int][]float64)
+
+	for _, t := range temp {
+
+		key := int(t/10) * 10
+		fmt.Println(key)
+
+		result[key] = append(result[key], t)
+	}
+	return result
 }
